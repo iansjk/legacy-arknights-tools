5b086651ace4d6d34de860b4d0171fc505f0b6d6
Ian Kim <iansjk@gmail.com>
2021-08-17 00:20:42 -0400

    Add some basic styling to OperatorGoalCard

--- src/components/OperatorGoalCard.tsx
+++ src/components/OperatorGoalCard.tsx
@@ -1,24 +1,57 @@
 import React, { useContext } from "react";
-import FocusedIcon from "@material-ui/icons/Star";
-import NotFocusedIcon from "@material-ui/icons/StarBorder";
+import FocusIcon from "@material-ui/icons/Star";
+import UnfocusIcon from "@material-ui/icons/StarBorder";
 import CompleteGoalIcon from "@material-ui/icons/CheckCircle";
 import DeleteGoalIcon from "@material-ui/icons/Cancel";
-import { Paper, Typography } from "@material-ui/core";
+import { IconButton, makeStyles, Paper, Typography } from "@material-ui/core";
 import { OperatorGoalState, OperatorGoalType } from "../store/goalsSlice";
 import PlannerContext from "./PlannerContext";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "list-style-type": "none",
+    padding: theme.spacing(1),
+    display: "flex",
+    alignItems: "center",
+  },
+  nameAndGoal: {
+    flexGrow: 1,
+  },
+  name: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 export type OperatorGoalCardProps = OperatorGoalState;
 
 const OperatorGoalCard: React.VFC<OperatorGoalCardProps> = (props) => {
-  const { operatorId, goal } = props;
+  const { operatorId, goal, focused } = props;
   const { operatorMap } = useContext(PlannerContext);
   const operator = operatorMap[operatorId];
+  const classes = useStyles();
+
   return (
-    <Paper elevation={3}>
-      <Typography component="span" variant="h6">
-        {operator.name}
-      </Typography>
-      {OperatorGoalType[goal]}
+    <Paper elevation={3} component="li" classes={classes}>
+      <span className={classes.nameAndGoal}>
+        <Typography component="span" variant="h6" className={classes.name}>
+          {operator.name}
+        </Typography>
+        <Typography component="span" variant="body1">
+          {OperatorGoalType[goal]}
+        </Typography>
+      </span>
+      <IconButton
+        aria-label={`${focused ? "Unfocus" : "Focus"} this goal`}
+        size="small"
+      >
+        {focused ? <UnfocusIcon /> : <FocusIcon />}
+      </IconButton>
+      <IconButton aria-label="Complete this goal" size="small">
+        <CompleteGoalIcon />
+      </IconButton>
+      <IconButton aria-label="Delete this goal" size="small">
+        <DeleteGoalIcon />
+      </IconButton>
     </Paper>
   );
 };
