7e666d4e71a4d8437b0881775d244c7fab828d74
Ian Kim <iansjk@gmail.com>
2021-08-29 17:55:06 -0400

    Implement new OperatorGoalIconography

--- src/components/OperatorGoalIconography.stories.tsx
+++ src/components/OperatorGoalIconography.stories.tsx
@@ -4,11 +4,16 @@ import OperatorGoalIconography, {
   OperatorGoalIconographyProps,
 } from "./OperatorGoalIconography";
 import { OperatorGoalType } from "../store/goalsSlice";
+import operatorData from "../data/operators.json";
 
 export default {
   title: "Planner/OperatorGoalIconography",
   component: OperatorGoalIconography,
   argTypes: {
+    operatorId: {
+      options: operatorData.map((op) => op.id),
+      control: { type: "select" },
+    },
     goal: {
       options: OperatorGoalType,
       control: { type: "select" },
@@ -19,32 +24,39 @@ export default {
 const Template: Story<OperatorGoalIconographyProps> = (args) => (
   <OperatorGoalIconography {...args} />
 );
+const baseArgs = {
+  operatorId: "char_002_amiya",
+};
+
 export const Elite1 = Template.bind({});
 Elite1.args = {
+  ...baseArgs,
   goal: OperatorGoalType["Elite 1"],
 };
 
 export const Elite2 = Template.bind({});
 Elite2.args = {
+  ...baseArgs,
   goal: OperatorGoalType["Elite 2"],
 };
 
 export const Skill1Mastery1 = Template.bind({});
 Skill1Mastery1.args = {
+  ...baseArgs,
   goal: OperatorGoalType["Skill 1 Mastery 1"],
 };
 
 export const Skill2Mastery2 = Template.bind({});
 Skill2Mastery2.args = {
+  ...baseArgs,
   goal: OperatorGoalType["Skill 2 Mastery 2"],
 };
 
 export const Skill3Mastery3 = Template.bind({});
 Skill3Mastery3.args = {
+  ...baseArgs,
   goal: OperatorGoalType["Skill 3 Mastery 3"],
 };
 
 export const SkillLevel = Template.bind({});
-SkillLevel.args = {
-  goal: OperatorGoalType["Skill Level 4 → 5"],
-};
+SkillLevel.args = { ...baseArgs, goal: OperatorGoalType["Skill Level 4 → 5"] };
--- src/components/OperatorGoalIconography.tsx
+++ src/components/OperatorGoalIconography.tsx
@@ -1,14 +1,59 @@
-import React from "react";
-import { OperatorGoalType } from "../store/goalsSlice";
+import React, { useContext } from "react";
+import { Box } from "@material-ui/core";
+import { eliteImageSrc, masteryImageSrc, skillImageSrc } from "../images";
+import { OperatorGoalState, OperatorGoalType } from "../store/goalsSlice";
+import PlannerContext from "./PlannerContext";
 
-export interface OperatorGoalIconographyProps {
-  goal: OperatorGoalType;
-}
+const ICON_SIZE = 30;
+
+export type OperatorGoalIconographyProps = Omit<OperatorGoalState, "focused">;
 
 const OperatorGoalIconography: React.VFC<OperatorGoalIconographyProps> = (
   props
 ) => {
-  const { goal } = props;
-  return <></>;
+  const { operatorId, goal } = props;
+  const { operatorMap } = useContext(PlannerContext);
+
+  switch (goal) {
+    case OperatorGoalType["Elite 1"]:
+    case OperatorGoalType["Elite 2"]:
+      return (
+        <Box component="span" aria-hidden="true" lineHeight={0}>
+          <img
+            src={eliteImageSrc(goal === OperatorGoalType["Elite 1"] ? 1 : 2)}
+            alt=""
+            height={ICON_SIZE}
+          />
+        </Box>
+      );
+    case OperatorGoalType["Skill 1 Mastery 1"]:
+    case OperatorGoalType["Skill 1 Mastery 2"]:
+    case OperatorGoalType["Skill 1 Mastery 3"]:
+    case OperatorGoalType["Skill 2 Mastery 1"]:
+    case OperatorGoalType["Skill 2 Mastery 2"]:
+    case OperatorGoalType["Skill 2 Mastery 3"]:
+    case OperatorGoalType["Skill 3 Mastery 1"]:
+    case OperatorGoalType["Skill 3 Mastery 2"]:
+    case OperatorGoalType["Skill 3 Mastery 3"]: {
+      const skillIndex = Math.floor(
+        (goal - OperatorGoalType["Skill 1 Mastery 1"]) / 3
+      );
+      const masteryLevel =
+        ((goal - OperatorGoalType["Skill 1 Mastery 1"]) % 3) + 1;
+      const { iconId, skillId } = operatorMap[operatorId].skills[skillIndex];
+      return (
+        <Box component="span" aria-hidden="true" lineHeight={0}>
+          <img src={skillImageSrc(iconId, skillId)} alt="" height={ICON_SIZE} />
+          <img
+            src={masteryImageSrc(masteryLevel as 1 | 2 | 3)}
+            alt=""
+            height={ICON_SIZE}
+          />
+        </Box>
+      );
+    }
+    default:
+      return null;
+  }
 };
 export default OperatorGoalIconography;
